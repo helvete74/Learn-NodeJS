380aa283ebf7ae1845fea966857e6df3f52936e0
KuboCar <patrick.romand@gmail.com>
2023-05-18 16:36:01 +0200

    premiers middleware

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,19 @@ const express = require('express'); //import de express
 
 const app = express(); //creation de l'application app de type express
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requete a bien été reçue !'})
-})
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    res.json({ message: 'Votre requete a bien été reçue !'});
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Réponse renvoyée avec succès !');
+});
 
 module.exports = app;
